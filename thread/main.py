e02adff8179671b155dcdb4fe3845ba50c4457dc
刘飞鹤 <feihe.liu@lebai.ltd>
2020-12-24 16:31:54 +0800

    thread

--- thread/main.py
+++ thread/main.py
@@ -2,7 +2,22 @@ from threading import Thread,Timer
 from time import sleep
 
 time_count = 0
+thread_count = 0
 
+def thread1_run(*args):
+    global thread_count
+    while True:
+        sleep(0.1)
+        print("t1, {0}, {1}".format(args, thread_count))
+        thread_count = thread_count+1
+
+def thread2_run(*args):
+    global thread_count
+    while True:
+        sleep(0.1)
+        print("t2 {0}, {1}".format(args, thread_count))
+        thread_count = thread_count+1
+    
 def timer_run():
     global time_count
     print('time')
@@ -18,7 +33,7 @@ def run(*args):
     timer_run()
     print('run done')
 
-def main():
+def test_args():
     count = 0
     thread = None
     a = [1]
@@ -32,6 +47,15 @@ def main():
             thread.start()
         if count>=100:
             break
+
+def test_threads():
+    t1 = Thread(target=thread1_run)
+    t1.start()
+    t2 = Thread(target=thread2_run)
+    t2.start()
+
+def main():
+    test_threads()
 
 if __name__ == "__main__":
     main()
