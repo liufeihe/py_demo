9e60681f0e21a7711960f068d5fae20ead64dcbe
刘飞鹤 <feihe.liu@lebai.ltd>
2021-01-06 16:36:32 +0800

    失败后1s，重连

--- grpc/main.py
+++ grpc/main.py
@@ -1,8 +1,11 @@
 from time import sleep
+from threading import Thread
 import grpc
 import rcs_pb2
 import rcs_pb2_grpc
 from google.protobuf.empty_pb2 import Empty
+
+rcs = None
 
 def generate_cmd(cmd):
     while True:
@@ -13,8 +16,28 @@ def print_io(io_type, ios):
     for io in ios:
         print('{0}, pin: {1}, value: {2}'.format(io_type, io.pin, io.value))
 
+def read_robot_ios():
+    global rcs
+    print('read robot ios')
+    try:
+        num = 0
+        res = rcs.GetRobotIOData(generate_cmd(rcs_pb2.RobotDataCmd()))
+        for r in res:
+            num = num+1
+            print(num)
+            print_io('di', r.robotDIOIn)
+            print_io('d0', r.robotDIOOut)
+            print_io('ai', r.robotAIOIn)
+            print_io('ao', r.robotAIOOut)
+    except Exception as e:
+        print(e)
+        sleep(1)
+        t = Thread(target=read_robot_ios)
+        t.start()
+    
+
 def run():
-    num = 0
+    global rcs
     print('create channel')
     channel = grpc.insecure_channel('192.168.3.227:5181')
     rcs = rcs_pb2_grpc.RobotControllerStub(channel)
@@ -25,15 +48,9 @@ def run():
     # res = rcs.GetGravity(Empty())
     # if res is not None:
     #     print('x:{0}, y:{1}, z:{2}'.format(res.x, res.y, res.z))
-
-    res = rcs.GetRobotIOData(generate_cmd(rcs_pb2.RobotDataCmd()))
-    for r in res:
-        num = num+1
-        print(num)
-        print_io('di', r.robotDIOIn)
-        print_io('d0', r.robotDIOOut)
-        print_io('ai', r.robotAIOIn)
-        print_io('ao', r.robotAIOOut)
+    t = Thread(target=read_robot_ios)
+    t.start()
+    
         
     print('end')
 
